528d22db10ac4fc5c804bf4a2a4df6cfa499e128
Cogitandi <p.kapica6@gmail.com>
2020-03-27 21:00:07 +0100

    rewirte jquery functions to parcels table (optimize)

--- public/tableFilter.js
+++ public/tableFilter.js
@@ -1,3 +1,205 @@
+$(document).ready(function() {
+    var $fuelSelect = $('#fuelSelect');
+    var $arimrSelect = $('#arimrOperatorSelect');
+    var $cropSelect = $('#plantNameSelect');
+    
+    $fuelSelect.change(function () {
+        tableActions();
+    });
+    $arimrSelect.change(function () {
+        tableActions();
+    });
+    $cropSelect.change(function () {
+        tableActions();
+    });
+    tableActions();
+    
+})
+
+function tableActions() {
+    removeOldTableIfExist();
+    addFilteredTBody();
+    scaleFieldNames();
+    scalePlantNames();
+    colorFields();
+    addSummaryRow();
+    addAreaToFields();
+    renumerate();
+}
+function removeOldTableIfExist() {
+    $oldTable = $('#parcels').find('tbody[filteredTable]')
+     if( $oldTable.length ) {
+        $oldTable.remove();
+    }
+}
+function isValueMatched($required, $value) {
+    if($required == 'all') return true;
+    if($required == $value) 
+        return true;
+    return false
+}
+function getFilteredTBody($isFuel, $arimrOperator, $plantName) {
+    removeOldTableIfExist();
+    
+    $data = $('#parcels').find('tbody[data]');
+    $outputTable = '<tbody filteredTable>';
+
+    $data.find('tr').each( function() {
+        $rowFuel = $(this).find('td[name=fuel]').text();
+        $rowArimrOperator = $(this).find('td[name=arimrOperator]').text();
+        $rowPlantName = $(this).find('td[name=plantName]').text();
+        if(isValueMatched($isFuel,$rowFuel) &&
+           isValueMatched($arimrOperator,$rowArimrOperator) &&
+           isValueMatched($plantName,$rowPlantName)) {
+            $outputTable += '<tr>'+$(this).html()+'</tr>';
+        }
+    })
+    $outputTable += '</tbody>';
+    return $outputTable;
+    
+}
+function addFilteredTBody() {
+        $choosedFuel = $('#fuelSelect').val();
+        $choosedArimr = $('#arimrOperatorSelect').val();
+        $choosedCrop = $('#plantNameSelect').val();
+        $newTBody = getFilteredTBody($choosedFuel,$choosedArimr,$choosedCrop);
+        $('#parcels > tbody').after($newTBody);
+}
+function amountOfParcelsByFieldName($fieldName) {
+    $counter = 0;
+    $counter = $('tbody[filteredTable]').find('td[name=fieldName]:contains('+ $fieldName +')').length;
+    return $counter;
+}
+function amountOfParcelsByPlantName($plantName) {
+    $counter = 0;
+    $counter = $('tbody[filteredTable]').find('td[name=plantName]:contains('+ $plantName +')').length;
+    return $counter;
+}
+function scaleFieldNames() {
+    $fieldsTD = $('tbody[filteredTable]').find('td[name=fieldName]');
+    $previousFieldName = null;
+    $fieldsTD.each(function() {
+        $thisFieldNameTD = $(this);
+        if($previousFieldName == $thisFieldNameTD.text()) {
+            $(this).hide();
+        } else {
+            $thisFieldNameTD.attr('rowspan', amountOfParcelsByFieldName($thisFieldNameTD.text()));
+            $previousFieldName = $thisFieldNameTD.text();
+        }
+        
+        
+    })
+}
+function scalePlantNames() {
+    $plantsTD = $('tbody[filteredTable]').find('td[name=plantName]');
+    $previousPlantName = null;
+    $plantsTD.each(function() {
+        $thisPlantNameTD = $(this);
+        if($previousPlantName == $thisPlantNameTD.text()) {
+            $(this).remove();
+        } else {
+            $thisPlantNameTD.attr('rowspan', amountOfParcelsByPlantName($thisPlantNameTD.text()));
+            $previousPlantName = $thisPlantNameTD.text();
+        }
+        
+        
+    })
+}
+function getArrayWithFieldNames() {
+    $fieldsTD = $('tbody[filteredTable]').find('td[name=fieldName]'); 
+    $fields = [];
+    $fieldsTD.each(function() {
+                   $fieldName = $(this).text();
+        if($.inArray($fieldName,$fields) == -1) {
+            $fields.push($fieldName);
+        }}
+    )
+    return $fields;
+}
+function colorFields() {
+    $TDfields = $('tbody[filteredTable]');
+    $even = false;
+    $fields = getArrayWithFieldNames();
+    
+    for(i=0;i<$fields.length;i++) {
+        if($even) {        
+            // Even row
+            $TDfields.find('td[name=fieldName]:contains('+$fields[i]+')').each(function() {
+                $(this).closest('tr').css('background-color', '#E8E8E8');
+            })
+            $even = false;
+        } else {
+            // Odd row
+            $TDfields.find('td[name=fieldName]:contains('+$fields[i]+')').each(function() {
+                //$(this).closest('tr').css('background-color', '#FFF');
+            })
+            $even = true;
+        }
+           
+        }
+    }
+function getFieldArea($fieldName) {
+    $area = 0;
+    //console.log($('tbody[filteredTable]').find('td[name=fieldName]:contains('+$fieldName+')').siblings()); 
+    $('tbody[filteredTable]').find('tr').each(function() {
+        if( $(this).find('td[name=fieldName]').text() == $fieldName) {
+            $area += $(this).find('td[name=area]').text()*100;
+        }
+        
+    })
+    return $area/100;
+}
+function getTotalArea() {
+    $area = 0;
+    $('tbody[filteredTable]').find('td[name=area]').each(function() {
+        $area += $(this).text()*100;
+    })
+    return $area/100;
+}
+function addSummaryRow() {
+    $html = '<tr>';
+    $html += '<td></td>';
+    $html += '<td></td>';
+    $html += '<td class="font-weight-bold">Razem</td>';
+    $html += '<td class="font-weight-bold">'+getTotalArea()+'</td>';
+    $html += '<td></td>';
+    $html += '<td></td>';
+    $html += '<td></td>';
+    $html += '</tr>';
+    $('tbody[filteredTable]').append($html);
+}
+function addAreaToFields() {
+    $('tbody[filteredTable]').find('td[name=fieldName]').each(function() {
+        $(this).append('<br />['+getFieldArea($(this).text())+' ha]');
+    })
+}
+function renumerate() {
+    $counter = 1;
+    $('tbody[filteredTable]').find('td[name=no]').each(function() {
+        $(this).html($counter);
+        $counter = $counter + 1;
+    })
+}
+    
+
+//    
+//    $fieldsTD.each(function() {
+//        if($even) {
+//            $fieldsTD.find(':contains('+$(this).text()+')').each(function() {
+//                $(this).closest('tr').css('background-color', '#E8E8E8');
+//            })
+//            $even = false;
+//        } else {
+//            $fieldsTD.find(':contains('+$(this).text()+')').each(function() {
+//                 $(this).closest('tr').css('background-color', '#FFF');
+//            })
+//           
+//            $even = true;
+//        }
+//    })
+
+
+/*
 $(document).ready(function () {
     var $fuelSelect = $('#fuelFilter');
     var $arimrSelect = $('#arimrFilter');
@@ -274,5 +476,5 @@ function colorField() {
 }
 
 
-
+*/
 
